383a87db6d3a84ed8545650f002d256c2bdfac0b
Correia, Jose Bastos <josebcorreia@deloitte.pt>
2023-06-30 14:52:44 +0100

    loadImage

--- Sources/HTTPService/HTTPService.swift
+++ Sources/HTTPService/HTTPService.swift
@@ -1,10 +1,46 @@
-public class HTTPService {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import UIKit
+import Combine
 
+public class HTTPService {
+    
+    var cancellables = Set<AnyCancellable>()
+    
     public init() {
+        
     }
     
-    public func printHello() -> String {
-        "print()"
+    public func loadImage(url: String) -> UIImage {
+        var image: UIImage = UIImage(systemName: "camera")!
+        requestImage(url: url)
+            .receive(on: RunLoop.main)
+            .replaceError(with: image)
+            .sink { value in
+                image = value!
+            }
+            .store(in: &cancellables)
+        return image
     }
+    
+    private func requestImage(url: String) -> AnyPublisher<UIImage?, HttpError> {
+        let url = URL(string: url)!
+        let urlRequest = URLRequest(url: url)
+        let route = ImageLoader()
+        route.endPoint = urlRequest
+        let http = RequestService()
+        return http.makeRequest(route)
+    }
+    
+    public func requestJson() {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
---
+++ Sources/HTTPService/NetworkProcessor.swift
@@ -0,0 +1,61 @@
+//
+//  File.swift
+//  
+//
+//  Created by Correia, Jose Bastos on 30/06/2023.
+//
+
+import Foundation
+import UIKit
+import Combine
+
+class RouteEndPoint<Response> {
+    var endPoint: URLRequest?
+    var parser: ((Data) -> Response)!
+}
+
+class ImageLoader: RouteEndPoint<UIImage?> {
+    
+    override init() {
+        super.init()
+        self.parser = {(data: Data) -> UIImage in
+            return UIImage(data: data) ?? UIImage()
+        }
+    }
+}
+
+class ServiceLoader<T: Codable>: RouteEndPoint<T> {
+    override init() {
+        super.init()
+        self.parser = {(data: Data) -> T in
+            return try! JSONDecoder().decode(T.self, from: data)
+        }
+    }
+}
+
+protocol RequestServiceProtocol {
+    func makeRequest<Response>(_ request: RouteEndPoint<Response>) -> AnyPublisher<Response, HttpError>
+}
+
+class RequestService: RequestServiceProtocol {
+    
+    func makeRequest<Response>(_ request: RouteEndPoint<Response>) -> AnyPublisher<Response, HttpError> {
+        return URLSession.shared.dataTaskPublisher(for: request.endPoint!)
+                .tryMap { (data, _) -> Response in
+                    guard let parser = request.parser else {
+                        return HttpError.parsing as! Response
+                    }
+                    return parser(data)
+                }
+                .mapError { error -> HttpError in
+                    switch error {
+                    case URLError.cannotFindHost:
+                        return HttpError.generic(error.localizedDescription)
+                    default:
+                        return HttpError.noData
+                        
+                    }
+                }
+                .eraseToAnyPublisher()
+        }
+}
