e47de2cf0323ce6a658f7fbf7f360860db708785
Correia, Jose Bastos <josebcorreia@deloitte.pt>
2023-07-13 15:25:47 +0100

    minor fix

--- Sources/HTTPService/HTTPService.swift
+++ Sources/HTTPService/HTTPService.swift
@@ -82,7 +82,7 @@ extension HTTPService {
     /// Method that handle any HTTP Request
     /// - Parameter urlRequest: urlRequest setted that enable you to make your request
     /// - Returns: Return a Publisher that could be of type Data or Error if there is any connection problem, that Data still needs to be transformed on the cliente side.
-    public func processRequest<T>(endPoint: EndPoint, responseDataType: T.Type) -> AnyPublisher<T, Error> {
+    public func processRequest<T>(endPoint: Route, responseDataType: T.Type) -> AnyPublisher<T, Error> {
         let decoder = RouteEndPoint<T>()
         decoder.endPoint = .products
         return RequestService.makeRequest(decoder)
